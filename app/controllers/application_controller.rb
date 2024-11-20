9294c7e052c2991ea865ebe0a938cba2cbff0fdd
mickykate <ka12miyuki@gmail.com>
2024-11-21 02:56:02 +0900

    deviseにusenameカラムを追加。NoMethodErrorが表示された。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  # ここから
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email]) # 新規登録時(sign_up時)にusenameというキーのパラメーターを追加で許可する
+  end
+  #ここまでを追記　usernameカラムをdbに登録させる設定
 end
