da90b45ec994b502a8f60eed920ba9dac0fb46b3
mickykate <ka12miyuki@gmail.com>
2024-11-21 14:02:01 +0900

    ログイン後のurl先を修正およびtopページ画像を表示するためにコードを修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,5 +7,14 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email]) # 新規登録時(sign_up時)にusenameというキーのパラメーターを追加で許可する
   end
+
+  def after_sign_in_path_for(resource)
+    top_show_path
+  end
+
+  private
+      def sign_in_required
+          redirect_to new_user_session_url unless user_signed_in?
+      end
   #ここまでを追記　usernameカラムをdbに登録させる設定
 end
--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,4 +1,6 @@
 class TopController < ApplicationController
+  before_action :sign_in_required, only: [:show]
+  
   def index
   end
 
